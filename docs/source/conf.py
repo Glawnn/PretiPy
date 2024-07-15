32f2a981c8776b3a4411bd69999bcf9cb48ffb4a
Vodkas <prungael@gmail.com>
2024-07-15 10:21:27 +0200

    doc: push doc on githubpages

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.githubpages',
+    'sphinx_rtd_theme',
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
